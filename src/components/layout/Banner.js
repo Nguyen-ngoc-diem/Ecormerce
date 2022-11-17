9238a4baf57e547e7bdd696c93861c7a1c56df44
Nguyen Thi Ngoc Diem <nguyenthingocdiem211@gmail.com>
2022-11-18 00:29:43 +0700

    Product

--- src/components/layout/Banner.js
+++ src/components/layout/Banner.js
@@ -10,14 +10,14 @@ const Banner = () => {
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="https://thietkehaithanh.com/wp-content/uploads/2013/06/thietkehaithanh-banner-my-pham-1.jpg" alt="First slide" />
+                    <img class="d-block w-100" src="/images/banner_1.png" alt="First slide" />
 
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="https://adsplus.vn/wp-content/uploads/2018/12/nguyen-tac-thiet-ke-banner-pham-dep-ngat-ngay-08-1.jpg" alt="Second slide" />
+                    <img class="d-block w-100" src="/images/banner_1.png" alt="Second slide" />
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="https://thietkehaithanh.com/wp-content/uploads/2013/06/thietkehaithanh-banner-my-pham-1.jpg" alt="Third slide" />
+                    <img class="d-block w-100" src="/images/banner_1.png" alt="Third slide" />
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
